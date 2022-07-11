f1ea2950c03250116e36d61aa43ab31432504975
Hakeem <64858288+Super-Nim@users.noreply.github.com>
2022-07-11 07:39:26 +0100

    updated email

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,7 @@ const Social = () => {
         <div className="social">
             <h2>Contact</h2>
 
-            <Button variant="contained" href="mailto:mwo248@yahoo.com" className="button">mwo248@yahoo.com</Button>
+            <Button variant="contained" href="mailto:keemwhitmore98@gmail.com" className="button">keemwhitmore98@gmail.com</Button>
             <Button variant="contained" href="https://www.linkedin.com/in/hakeem-whitmore/" className="button">LinkedIn</Button>
             <Button variant="contained" href="https://github.com/Super-Nim" className="button">GitHub</Button>
         </div>
